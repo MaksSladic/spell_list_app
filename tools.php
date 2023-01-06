05ec9eeec8b9ce67ea64414667ceb11ab02ce027
Maks Sladič <maks.sladic@gmail.com>
2023-01-06 13:38:54 +0100

    jwt added

--- tools.php
+++ tools.php
@@ -281,5 +281,53 @@ function delete_all_char_of_user($IDofUser)
 	mysqli_query($zbirka, $poizvedba);
 }
 
+// --------------------------------------------------------------------
+// JWT
+
+function generate_jwt($headers, $payload, $secret = 'd4d6d8d10d12d20') {
+	$headers_encoded = base64url_encode(json_encode($headers));
+	
+	$payload_encoded = base64url_encode(json_encode($payload));
+	
+	$signature = hash_hmac('SHA256', "$headers_encoded.$payload_encoded", $secret, true);
+	$signature_encoded = base64url_encode($signature);
+	
+	$jwt = "$headers_encoded.$payload_encoded.$signature_encoded";
+	
+	return $jwt;
+}
+
+
+function base64url_encode($str) {
+    return rtrim(strtr(base64_encode($str), '+/', '-_'), '=');
+}
+
+
+function is_jwt_valid($jwt, $secret = 'd4d6d8d10d12d20') {
+	// split the jwt
+	$tokenParts = explode('.', $jwt);
+	$header = base64_decode($tokenParts[0]);
+	$payload = base64_decode($tokenParts[1]);
+	$signature_provided = $tokenParts[2];
+
+	// check the expiration time - note this will cause an error if there is no 'exp' claim in the jwt
+	$expiration = json_decode($payload)->exp;
+	$is_token_expired = ($expiration - time()) < 0;
+
+	// build a signature based on the header and payload using the secret
+	$base64_url_header = base64url_encode($header);
+	$base64_url_payload = base64url_encode($payload);
+	$signature = hash_hmac('SHA256', $base64_url_header . "." . $base64_url_payload, $secret, true);
+	$base64_url_signature = base64url_encode($signature);
+
+	// verify it matches the signature provided in the jwt
+	$is_signature_valid = ($base64_url_signature === $signature_provided);
+	
+	if ($is_token_expired || !$is_signature_valid) {
+		return FALSE;
+	} else {
+		return TRUE;
+	}
+}
 
 ?>
--- web_page/index.php
+++ web_page/index.php
@@ -6,7 +6,7 @@
     <title> home </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
-      button 
+        button 
         {   
             background-color: #4CAF50;   
             width: 100%;  
@@ -16,11 +16,22 @@
             border: none;   
             cursor: pointer;   
         }  
-      Body
-      {
-        font-family: Calibri, Helvetica, sans-serif;  
-        background-color: #CD9C8A;  
-      }
+        Body
+        {
+            font-family: Calibri, Helvetica, sans-serif;  
+            background-color: #CD9C8A;  
+        }
+        .imgbox 
+        {
+            display: grid;
+            height: 100%;
+        }   
+        .center-fit 
+        {
+            max-width: 100%;
+            max-height: 100vh;
+            margin: auto;
+        }
       
     </style>
   </head>
@@ -53,6 +64,11 @@
                 </Form>               
             </div>
         </div>
+
+        <div class="imgbox">
+            <img class="center-fit" src='DnD-Logo.png'>
+        </div>
+
     </div>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
